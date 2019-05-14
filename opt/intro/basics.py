d655b3ec2f82684c84b6c5e7152a70a889d2e945
Alan Weber <pinkshrub@gmail.com>
2019-05-14 16:53:33 -0700

    added day 1 things

---
+++ opt/intro/basics.py
@@ -0,0 +1,49 @@
+# Print 1-255 
+# Print all the integers from 1 to 255.
+
+def print_1_to_255(): #you may need to define parameters!
+    "print all the integers from 1 to 255" #optional doc string explains what function does
+    # your code here
+    pass # or return! who knows!
+
+# Print Ints and Sum 0-255 
+# Print integers from 0 to 255, and the sum so far.
+
+# Find and Print Max 
+# Print the largest element in a given array. 
+
+# Print Odds 1-255 
+# Prints all odd integers from 1 to 255
+
+# Array with Odds
+# Create an array with odd integers from 1-255.
+
+# Iterate and Print Array 
+# Print all values in a given array. 
+
+# Get and Print Average 
+# Analyze an array’s values and print the average. 
+
+# Greater than Y 
+# Count and print the number of array values less than a given Y. 
+
+# Max, Min, Average 
+# Given an array, print max, min and average values. 
+
+# Square the Values 
+# Given an array, square each value in the array.  
+
+# Zero Out Negative Numbers 
+# Set negative array values to zero.  
+
+# Shift Array Values
+# Shift array values: drop the first and leave '0' at end.  
+
+# Swap String for Array Negative Values
+# Replace any negative array values with 'Dojo'. 
+
+
+
+
+
+if __name__ == "__main__":
